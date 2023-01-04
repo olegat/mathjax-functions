71aeb17cd04f0ae49fc6e20496747b3cab85b5c7
Oli Legat <olegat@google.com>
2023-01-04 13:29:34 +0000

    Fix eslint errors

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 const functions = require("firebase-functions");
 
-const {mathjax}             = require('mathjax-full/js/mathjax.js');
-const {TeX}                 = require('mathjax-full/js/input/tex.js');
-const {SVG}                 = require('mathjax-full/js/output/svg.js');
-const {liteAdaptor}         = require('mathjax-full/js/adaptors/liteAdaptor.js');
-const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
+const {mathjax}             = require("mathjax-full/js/mathjax.js");
+const {TeX}                 = require("mathjax-full/js/input/tex.js");
+const {SVG}                 = require("mathjax-full/js/output/svg.js");
+const {liteAdaptor}         = require("mathjax-full/js/adaptors/liteAdaptor.js");
+const {RegisterHTMLHandler} = require("mathjax-full/js/handlers/html.js");
 
 function convert(math, InOut) {
   const adaptor = liteAdaptor();
   RegisterHTMLHandler(adaptor);
-  const html    = mathjax.document('', InOut);
+  const html    = mathjax.document("", InOut);
   const output  = html.convert(math);
   const content = adaptor.outerHTML(output.children[0]);
   return content;
@@ -22,7 +22,7 @@ function convert(math, InOut) {
 
 exports.tex2svg = functions.https.onRequest((request, response) => {
   const math = decodeURIComponent(request._parsedUrl.query);
-  response.set('Content-Type', 'image/svg+xml');
+  response.set("Content-Type", "image/svg+xml");
   return response.send(convert(math, {
     InputJax: new TeX(),
     OutputJax: new SVG()
