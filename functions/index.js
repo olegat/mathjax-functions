1b8983065db198e3ec4ac7f7c78d38cb514e1ec7
Oli Legat <olegat@google.com>
2023-01-04 11:47:24 +0000

    Remove commented code

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 // -*- Mode: js; js-indent-level: 2 -*-
-//**
 // Based on github.com/mathjax/MathJax-demos-node/blob/master/direct/tex2svg
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -28,43 +27,3 @@ exports.tex2svg = functions.https.onRequest((request, response) => {
     OutputJax: new SVG()
   }));
 });
-//*/
-
-/**
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-
-const functions = require("firebase-functions");
-var   mathjax   = require("mathjax-node-sre");
-
-async function convert(math) {
-  mathjax.config({MathJax: {SVG: {font: "TeX"}}});
-  mathjax.start();
-  let input = {
-    math: math,
-    format: "TeX",
-    svg: true,
-  };
-  return await mathjax.typeset(input)
-      .then((data) => { return data.svg; })
-      .catch((err) => { return err; });
-  // mathjax.typeset({
-  //   math: math,
-  //   format: "TeX",
-  //   svg: true,
-  //   speakText: true,
-  //   ex: 6,
-  //   width: 100,
-  //   linebreaks: true
-  // }, function (data) {
-  //   return {console.log(data.svg)}
-  // });
-}
-
-exports.tex2svg = functions.https.onRequest((request, response) => {
-  const math = request._parsedUrl.query;
-  convert(math).then((output) => {
-    response.send(output);
-  });
-});
-//*/
